7207569ef8904fb0286a6d39c12dac8f0d632b36
Carlos Alvarado (from VM) <cealvara@gmail.com>
2017-05-19 15:39:40 -0500

    changes

--- mrjobs/test_bucket.py
+++ mrjobs/test_bucket.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
     client = storage.Client()
     bucket = client.get_bucket('data-cs123')
     blob = bucket.get_blob('pos_words.txt')
-    positive_words = blob.download_as_string().split("\n")
+    positive_words = blob.download_as_string().splitlines()
     print(positive_words)
