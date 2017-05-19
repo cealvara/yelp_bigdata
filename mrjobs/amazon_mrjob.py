cdf459d8f4df8f3bcc645b75a6acb62335a0aa23
tomjarosz <tjarosz@uchicago.edu>
2017-05-18 21:50:30 -0500

    add mrjob for review stats and sentiment

---
+++ mrjobs/amazon_mrjob.py
@@ -0,0 +1,79 @@
+from mrjob.job import MRJob
+import json
+import re
+
+WORD_RE = re.compile(r"[\w']+")
+
+pos_words = []
+with open("pos_words.txt", "r") as f:
+    for row in f:
+        pos_words.append(row.rstrip())
+pos_words = set(pos_words)
+
+neg_words = []
+with open("neg_words.txt", "r") as f:
+    for row in f:
+        neg_words.append(row.rstrip())
+neg_words = set(neg_words)
+
+
+class AmazonReviewReduce(MRJob):
+
+    def mapper(self, _, line):
+
+        line = json.loads(line)
+        
+        reviewer_id = line.get("reviewerID", "")
+        asin = line.get("asin", "")
+        reviewer_name = line.get("reviewerName", "")
+        helpful = line.get("helpful", "")
+        review_text = line.get("reviewText", "")
+        review_score = line.get("overall", "")
+        review_summary = line.get("summary", "")
+        unix_review_time = line.get("unixReviewTime", "")
+        review_time = line.get("reviewTime", "")
+
+        word_count = 0
+        pos_count = 0
+        neg_count = 0
+
+        for word in WORD_RE.findall(review_text):
+            word_count += 1
+            if word in pos_words:
+                pos_count += 1
+            if word in neg_words:
+                neg_count += 1
+
+        yield asin, [pos_count, neg_count, word_count, review_score, 1]
+
+        #to build out for time analysis-- pick products with the highest number of reviews 
+        #and set up a binary variable to calculate if a review has above some threshold of 
+        #positive reviews and/or above threshold of negative reviews. Create seasonal/monthly/
+        #yearly buckets in order to capture the time of the review to assess trends in number of 
+        #reviews, positive/negative reivews, etc. across the buckets. 
+
+    def reducer(self, product, info):
+
+        pos_count = 0
+        neg_count = 0
+        word_count = 0
+        review_score = 0
+        review_count = 0
+
+        for review in info:
+            pos_count += review[0]
+            neg_count += review[1]
+            word_count += review[2]
+            review_score += review[3]
+            review_count += review[4]
+
+            avg_score = review_score/review_count
+
+        #fix the reducer so that it doesn't iterate through a list
+        #query the metadata here to grab the price, categories, etc.
+
+        yield product, [pos_count, neg_count, word_count, avg_score, review_count]
+
+
+if __name__ == '__main__':
+    AmazonReviewReduce.run()
