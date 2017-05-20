f250609dd025e13ec09d9caba8290201ec3b7d05
Carlos Alvarado (from VM) <cealvara@gmail.com>
2017-05-20 16:03:06 -0500

    added file to manage disks

--- utilities/attach_and_mount.py
+++ utilities/attach_and_mount.py
@@ -15,7 +15,7 @@ def attach(instance, disk_name):
 def mount(instance):
     instance_name = instance['NAME']
 
-    subcommand = 'mount /dev/sdb /mnt/storage'
+    subcommand = 'sudo mount /dev/sdb /mnt/storage'
 
     command = "gcloud compute ssh {}".format(instance_name) + \
         " --command=' {}'".format(subcommand)
