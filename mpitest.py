a5d2a97c5fed1042ad4ae5219d799af80150d2f0
Carlos Alvarado (from VM) <cealvara@gmail.com>
2017-05-21 11:46:49 -0500

    changes

--- mpitest.py
+++ mpitest.py
@@ -7,15 +7,19 @@ rank = MPI.COMM_WORLD.Get_rank()
 name = MPI.Get_processor_name()
 
 DB_PATH = '/mnt/storage/metadata.db'
+NUMROWS = 9430088
+STEP = int(NUMROWS/size) + 1
 
 if __name__ == '__main__':
     
     conn = sqlite3.connect(DB_PATH)
 
     c = conn.cursor()
+    
+    offset = rank * STEP
 
-    asin_list = c.execute('''select asin from METADATA limit 10;''').fetchall()
-
+    asin_list = c.execute('''select asin from METADATA order by asin limit 2 offset ?;''', (offset)).fetchall()
+    
     for asin in asin_list:
         c.execute('''select count(*) from ALSOVIEWED where asin=?;''', (asin[0],))
 
