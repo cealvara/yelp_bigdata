d19d71ae717f01781fec45d61aa818704b7b46bf
Carlos Alvarado (from VM) <cealvara@gmail.com>
2017-05-27 11:02:44 -0500

    added download file script

---
+++ utilities/4_download_files.py
@@ -0,0 +1,40 @@
+import os
+import subprocess
+
+from 1_get_info_instances import get_info_instances
+
+def get_info_total_files():
+
+    subprocess.call('gsutil ls -l gs://data-cs123/products_by_category > info_files.txt', shell=True)
+    with open('info_files.txt', 'r') as f:
+        info_files = []
+        for line in f:
+            data = line.split()
+            print(len(data))
+
+            if len(data) == 3:
+                filesize = data[0]
+                filename = data[2]
+                info_files.append( (filename, filesize))
+
+    for info in info_files:
+        print(info)
+
+    subprocess.call('sudo rm info_files.txt', shell=True)
+    return info_files
+
+
+def download_files_into_vms():
+    pass
+
+
+if __name__ == '__main__':
+    instances = get_info_instances()
+
+    n_instances = len(instances)
+
+    info_files = get_info_total_files()
+
+    splits = get_splits(info_files)
+    
+    download_files_into_vms()
