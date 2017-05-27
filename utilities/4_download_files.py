a5ae121b27126c197c4de8be233108cc06d57f8d
Jacob Gaynor <jwgaynor@cs.uchicago.edu>
2017-05-27 11:53:46 -0500

    threading

--- utilities/4_download_files.py
+++ utilities/4_download_files.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-
+import threading
 from helper_get_info_instances import get_info_instances
 
 def get_info_total_files():
@@ -40,22 +40,40 @@ def get_splits(info_files, k):
 
     return nodes
 
+class myThread (thread.Thread):
+    def __init__(self, threadID, list_of_files):
+        threading.Thread.__init__(self):
+        self.threadID = threadID
+        self.files = list_of_files
+    def run(self):
+        for filename in list_of_files:
+            subcommand = 'gsutil cp {} ~/'.format(filename)
+            command = "gcloud compute ssh {}".format(instance_name) + \
+                " --command=' {}'".format(subcommand)
+            subprocess.call(command, shell=True)
+
 
 def download_files_into_vms(list_of_splits, instances):
+
+    threads = []
     
     for i, (totalsize, list_of_files) in enumerate(list_of_splits):
         instance_name = instances[i]['NAME']
 
-        for filename in list_of_files:
+        thread = myThread(i,list_of_files)
+        threads.append(thread)
 
-            subcommand = 'gsutil cp {} ~/'.format(filename)
+        # for filename in list_of_files:
+        #     subcommand = 'gsutil cp {} ~/'.format(filename)
+        #     command = "gcloud compute ssh {}".format(instance_name) + \
+        #         " --command=' {}'".format(subcommand)
+        #     subprocess.call(command, shell=True)
 
-            command = "gcloud compute ssh {}".format(instance_name) + \
-                " --command=' {}'".format(subcommand)
+    for thread in threads:
+        thread.start()
 
-            subprocess.call(command, shell=True)
-
-
+    for thread in threads:
+        thread.join()
 
 if __name__ == '__main__':
     instances = get_info_instances()
