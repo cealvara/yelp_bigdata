970dd8286f10bd5c651e94075e2b0d1148d1cd16
Carlos Alvarado (from VM) <cealvara@gmail.com>
2017-05-28 00:41:06 -0500

    changed code

--- mpi_codes/avg_and_sd.py
+++ mpi_codes/avg_and_sd.py
@@ -1,3 +1,4 @@
+import os
 import re
 import sqlite3
 import time
@@ -10,16 +11,15 @@ size = MPI.COMM_WORLD.Get_size()
 rank = MPI.COMM_WORLD.Get_rank()
 name = MPI.Get_processor_name()
 
-METADATA_JSON = '/mnt/storage/metadata.json'
+
 METADATA_DB = '/mnt/storage/metadata.db'
 
 # JACOB: PLEASE CREATE A NEW DB WITH TOM'S OUTPUT, CALLED "posneg.db"
 
-POSNEG_DB = '/mnt/storage/posneg.db'
-
-TOP_K_VALUE = 10
+POSNEG_DB = '/mnt/storage/reviews_analysis.db'
 
 ASIN_RE = re.compile(r"'asin': '(\w+)'")
+CAT_RE = re.compile(r"meta_(\w+).json")
 
 def get_file_ranges(fname, chunks):
     f = open(fname)
@@ -78,53 +78,82 @@ def get_values_for_avg(file_range):
     return outlist
 
 
-def get_values_for_sd(file_range, avg_by_category):
-    outlist = None
+def get_values_for_avg(filename):
+    '''
+    Reads a given file object and outputs
+    tot_sum, N and list of values for SD
+    '''
+    f = open(filename, 'r')
+    
+    conn = sqlite3.connect(POSNEG_DB)
 
-    #here, outlist should be a list of tuples with:
-    # (category name, sum of squared difference...)
-    return outlist
+    c_posneg = conn.cursor()
+
+    n = 0
+    tot_score = 0
+    tot_pos = 0
+    tot_neg = 0
+    list_score = []
+    list_pos = []
+    list_neg = []
+
+    for line in f:
+        asin = ASIN_RE.search(line).group(1)
+
+        #get more info from raw metadata?
+        
+        query = c_posneg.execute('''
+            select avg_score, total_prod_pos, total_prod_neg, total_prod_words 
+            from scores 
+            where asin=?;''', (asin,))
+        avg_score, total_prod_pos, total_prod_neg, total_prod_words = query.fetchone()
+        avg_pos = total_prod_pos / total_prod_words
+        avg_neg = total_prod_neg / total_prod_words
+
+        list_score.append(avg_score)
+        list_pos.append(avg_pos)
+        list_neg.append(avg_neg)
+
+        tot_score += avg_score
+        tot_pos += avg_pos
+        tot_neg += avg_neg
+
+        n += 1
+
+    f.close()
+
+    return n, tot_score, tot_pos, tot_neg, list_score, list_pos, list_neg
 
 
 if __name__ == '__main__':
     
+    metadata_json_files = [f for f in os.listdir() if '.json' in f]
+    
     if rank == 0:
         print(time.time())
-        file_ranges = get_file_ranges(METADATA_JSON, size)
-        print(file_ranges, " from machine ", rank)
-    else:
-        file_ranges = None
 
-    # each VM gets its own file_range to work with
-    file_range = comm.scatter(file_ranges, root=0)
-  
+    
+    # GET STATS BY CATEGORY
+    stat_by_category = {}
+    
+    for filename in metadata_json_files:
+        category = CAT_RE.search(filename).group(1)
+        stat_by_category[category] = {}
 
-#STEP 1. GET AVERAGE BY CATEGORY
+        # each VM processes the file_range to get a list of values
+        # (this is like a mapper)
+        n, tot_score, tot_pos, tot_neg, list_score, list_pos, list_neg = get_values_for_avg(filename)
 
-    # each VM processes the file_range to get a list of values
-    # (this is like a mapper)
-    list_of_keyvalues_for_avg = get_values_for_avg(file_range)
+        stat_by_category[category]['Mean'] = tot_score / n 
+
+        stat_by_category[category]['SD'] = sum(
+            [(l - tot_score / n) ** 2 for l in list_score]) / n
+
 
     #root VM gathers all the chunks
-    gathered_chunks_for_avg = comm.gather(list_of_keyvalues_for_avg, root=0)
-
+    gathered_stat = comm.gather(stat_by_category, root=0)
+    
     if rank == 0:
-        # process values from all chunks to get avg by category
-        avg_by_category = f(gathered_chunks_for_avg)
-
-#STEP 2. GET SD BY CATEGORY
-    list_of_keyvalues_for_sd = get_values_for_sd(file_range, avg_by_category)
-
-    #root VM gathers all the chunks
-    gathered_chunks_for_sd = comm.gather(list_of_keyvalues_for_sd, root=0)
-
-    if rank == 0:
-        # process values from all chunks to get sd by category
-        sd_by_category = g(gathered_chunks_for_sd)
-
-
-    if rank == 0:
+        print(gathered_stat)
         print(time.time())
-        print(avg_by_category)
-        print(sd_by_category)       
 
